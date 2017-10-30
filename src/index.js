dde080099e1f2e02a424c2c0755340107f0c78b0
广陵大琴渣 <jack.sha@zmlearn.com>
2017-10-30 19:00:06 +0800

    button basic style

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import { h, app } from "hyperapp";
+import picostyle from "picostyle";
+const style = picostyle(h);
+const Div = style("div")(({stys:{width, height, color,bcolor,hcolor,hbcolor}}) => ({
+    width: `${width||100}px`,
+    display: 'flex',
+    backgroundColor: `${bcolor||'lightgrey'}`,
+    cursor: 'pointer',
+    height: `${height||30}px`,
+    color: `${color||'black'}`,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '10px',
+    userSelect: 'none',
+    ":hover":{
+      backgroundColor:`${hbcolor||"red"}`,
+      color:`${hcolor||"pink"}`,
+    }
+    
+}));
+
+export const Button = ({stys, val, click }) => (
+  <Div stys={stys} onclick={e => click(e)}>
+    {val}
+  </Div>
+);
