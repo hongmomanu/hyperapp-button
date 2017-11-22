fe58f405d603770b00b8a1d5864da49a7a368e35
广陵大琴渣 <jack.sha@zmlearn.com>
2017-11-22 16:33:00 +0800

    upgrade to 0.16

--- README.md
+++ README.md
@@ -5,9 +5,8 @@
 
     ```
     import { Button } from "hyperapp-button"  // Install all dependencies
-    const style1 = {width:120, height:40, color:'lightskyblue', bcolor:'yellow',
-                        hcolor:'white',hbcolor:'blue',radius:'5',imgsrc:'/icons/add.svg'}  //custom styles
+    const btnclasses = "pointer white w4 bg-orange dim br4-ns b--dark-pink h2 grow"    //custom styles
 
-    <Button stys={style1}  val={"+"} click={a.add}></Button>  //use the function component                   
+    <Button stys={btnclasses}  val={"+"} click={a.add}></Button>  //use the function component                   
     ```
 
--- src/index.js
+++ src/index.js
@@ -1,41 +1,9 @@
 import { h, app } from "hyperapp";
-import picostyle from "picostyle";
-const style = picostyle(h);
-const Div = style("div")(({stys:{width, height, color,bcolor,hcolor,hbcolor,radius, imgsrc}}) => ({
-    width: `${width||100}px`,
-    display: 'flex',
-    flexDirection: 'row',
-    backgroundColor: `${bcolor||'lightgrey'}`,
-    cursor: 'pointer',
-    height: `${height||30}px`,
-    color: `${color||'black'}`,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: `${radius||8}px`,
-    userSelect: 'none',
-    ":hover":{
-      backgroundColor:`${hbcolor||"red"}`,
-      color:`${hcolor||"pink"}`,
-    }
-    
-}));
-const Icon = style("img")(({src}) => ({
-  //position: 'absolute',
-  width:'20px',
 
-
-}) )
-const Text = style("div")(()=>({
-  display:'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  flex:1
-}))
 
 export const  Button = ({stys={}, val, click }) => (
-  <Div stys={stys} onclick={e => click(e)}>
-    <Text>{(stys && stys.imgsrc)?<Icon src={stys.imgsrc} /> : ''}{val}</Text>
-    
-  </Div>
+  <div class={stys} onclick={e => click(e)}>
+    <div style={{display:'flex',alignItems:'center',justifyContent:'center',flex:1,height:'100%'}}>{(stys && stys.imgsrc)?<img src={stys.imgsrc} /> : ''}{val}</div>
+  </div>
 );
 
