75a2fab2f078f8531c75cfd41db1ab9ccf3dc26d
广陵大琴渣 <jack.sha@zmlearn.com>
2017-11-22 16:57:45 +0800

    remove img tag

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { h, app } from "hyperapp";
 
 
-export const  Button = ({stys={}, val, click }) => (
+export const  Button = ({stys={}, val, click}) => (
   <div class={stys} onclick={e => click(e)}>
-    <div style={{display:'flex',alignItems:'center',justifyContent:'center',flex:1,height:'100%'}}>{(stys && stys.imgsrc)?<img src={stys.imgsrc} /> : ''}{val}</div>
+    <div style={{display:'flex',alignItems:'center',justifyContent:'center',flex:1,height:'100%'}}>{val}</div>
   </div>
 );
 
